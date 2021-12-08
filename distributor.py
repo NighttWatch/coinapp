bf75a9da8586dde39983c16aa7f58efc95a630ca
NighttWatch <serdartekin0721@gmail.com>
2021-12-08 10:02:35 +0300

    local

--- distributor.py
+++ distributor.py
@@ -47,11 +47,10 @@ class planetSender:
                 if (len(datas["wallets"]) == i):
                     break
                 else:
-                    print(datas["wallets"][i]["passphrase"])
                     publicKey = datas["wallets"][i]["passphrase"]
                     privateKey = mnemonic.to_private_key(publicKey)
                     senderAddress = datas["wallets"][i]["address"] 
-                    algod_address = "http://localhost:4001"
+                    algod_address = "http://3.232.109.91:4001"
                     algod_token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
                     algod_client = algod.AlgodClient(algod_token, algod_address)
 
