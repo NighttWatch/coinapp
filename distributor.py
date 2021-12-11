0534c105f5411b550b2d1c3060ccfb350b16dee6
NighttWatch <serdartekin0721@gmail.com>
2021-12-11 16:38:37 +0300

    docker-local

--- distributor.py
+++ distributor.py
@@ -50,7 +50,7 @@ class planetSender:
                     publicKey = datas["wallets"][i]["passphrase"]
                     privateKey = mnemonic.to_private_key(publicKey)
                     senderAddress = datas["wallets"][i]["address"] 
-                    algod_address = "http://172.20.0.1:4001"
+                    algod_address = "http://172.21.0.1:4001"
                     algod_token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
                     algod_client = algod.AlgodClient(algod_token, algod_address)
 
